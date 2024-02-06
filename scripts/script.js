a51c6533848c0b5076e1509a5e0f5f8a03992c78
CattyCathy <152269217+ShimitsuTheLotus@users.noreply.github.com>
2024-02-06 14:30:31 +0800

    Update script.js

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-var GetRepoLanguageInfo = new XMLHttpRequest();
-GetRepoLanguageInfo.open('GET',"https://api.github.com/repos/ShimitsuTheLotus/Tamge_/languages");
-GetRepoLanguageInfo.onload = function(){
-    
-}
-GetRepoLanguageInfo.send();
-var LanguageBoard = document.getElementById("LanguageBoard");
-LanguageBoard.innerHTML = "text";
